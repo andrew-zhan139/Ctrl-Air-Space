ecc1f47401583d89e77827f5f0ac1ba88f414efb
Angela Wang <angelawang7e3@gmail.com>
2021-01-17 03:30:01 -0500

    Edit dropdown box width

--- src/js/components/DropDown.js
+++ src/js/components/DropDown.js
@@ -9,7 +9,7 @@ import Select from '@material-ui/core/Select';
 const useStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(1),
-    minWidth: 200,
+    minWidth: 250,
   },
   selectEmpty: {
     marginTop: theme.spacing(2),
