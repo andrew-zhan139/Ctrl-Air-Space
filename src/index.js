87f6a6607b9ec158d13a7eafaeb20edfb83c4e4f
Angela Wang <angelawang7e3@gmail.com>
2021-01-18 22:35:55 -0500

    edit webpack entry path

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import ActionCommand from './ActionCommand';
 
 import './index.scss';
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles"
@@ -12,8 +11,6 @@ const theme = createMuiTheme({
         type: "dark"
     }
 });
-
-// ReactDOM.render(<App />, document.getElementById('root'))
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/js/index.js',
+  entry: './src/index.js',
   devtool: 'inline-source-map',
   target: 'electron-renderer',
   module: {
